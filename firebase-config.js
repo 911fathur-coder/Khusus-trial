7b613c5cdb66e4f1044c585ba54f9b7082e73640
911fathur-coder <911fathur@gmail.com>
2026-07-05 23:27:20 +0700

    Update SHEETS_WEBAPP_URL to new endpoint

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,1 @@
-window.SHEETS_WEBAPP_URL = https://script.google.com/macros/s/AKfycbwK553tuG-ODcFOdI-4vsJar8ddDCvCz1Z0W-w6p9g8Xps-yb_UJixtPVzRnM1dc8IKgg/exec
+window.SHEETS_WEBAPP_URL = https://script.google.com/macros/s/AKfycbz0IFLwyQ8cOGTa3tMVCVurfSSCaCJVCKcdEFjr5l1URo-YLQ6YFxmAC-BlY3PxjGRO7A/exec
